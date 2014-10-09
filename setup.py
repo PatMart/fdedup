6dbf0ab095a4f6862329c9fe4ba076910931b325
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-09 23:02:24 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fdedup',
+    version='0.1',
+    packages=['fdedup'],
+    url='https://github.com/themalkolm/fdedup',
+    license='',
+    author='Alexander Krasnukhin',
+    author_email='the.malkolm@gmail.com',
+    description='File Deduplicator'
+)

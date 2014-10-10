fc1bb389aa12dbbc0e0cfbbd5e3e5e53bc4efe7d
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-11 00:45:29 +0200

    Add integration test for help.

---
+++ itests/test_help.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import unittest
+import subprocess
+
+
+class Test(unittest.TestCase):
+    def test_help(self):
+        with open(os.devnull, 'w') as devnull:
+            code = subprocess.call(['./fdedup/fdedup.py', '-h'], stdout=devnull)
+            self.assertEqual(0, code)
+
+    def test_help_long(self):
+        with open(os.devnull, 'w') as devnull:
+            code = subprocess.call(['./fdedup/fdedup.py', '--help'], stdout=devnull)
+            self.assertEqual(0, code)

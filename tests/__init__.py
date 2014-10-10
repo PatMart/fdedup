473227622cea2c3ccc82f7429084753b951e551b
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-11 00:07:36 +0200

    No need for this magic.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,1 @@
-import os,sys,inspect
-
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
+# -*- coding: utf-8 -*-

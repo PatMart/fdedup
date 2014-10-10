8e2089273972081ee3400f3e0f448a5999036e64
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-11 00:48:12 +0200

    Don't print out anything in integration tests.
    For now (!) we care about return code only.

--- itests/test_help.py
+++ itests/test_help.py
@@ -9,10 +9,10 @@ import subprocess
 class Test(unittest.TestCase):
     def test_help(self):
         with open(os.devnull, 'w') as devnull:
-            code = subprocess.call(['./fdedup/fdedup.py', '-h'], stdout=devnull)
+            code = subprocess.call(['./fdedup/fdedup.py', '-h'], stdout=devnull, stderr=devnull)
             self.assertEqual(0, code)
 
     def test_help_long(self):
         with open(os.devnull, 'w') as devnull:
-            code = subprocess.call(['./fdedup/fdedup.py', '--help'], stdout=devnull)
+            code = subprocess.call(['./fdedup/fdedup.py', '--help'], stdout=devnull, stderr=devnull)
             self.assertEqual(0, code)

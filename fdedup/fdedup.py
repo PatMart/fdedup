c4f10ff1f7be87f86b0348166cb9461323db6438
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-10 22:43:48 +0200

    Initialize logging with different verbosity levels.

--- fdedup/fdedup.py
+++ fdedup/fdedup.py
@@ -5,6 +5,7 @@ import argparse
 import functools
 
 import hashlib
+import logging
 
 import os
 import itertools
@@ -47,8 +48,24 @@ def main():
         description='Find file duplicates.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('paths', nargs='+', metavar='PATH', help='paths to scan for duplicates')
+
+    verbosity = parser.add_mutually_exclusive_group()
+    verbosity.add_argument('-v', '--verbose', action='count', default=0, help='be verbose')
+    verbosity.add_argument('-q', '--quiet', action='store_true', help='be quiet')
+
     parser.add_argument('--hash', choices=hashlib.algorithms, default='md5', help='hash algorithm to use')
     opts = parser.parse_args()
+
+    log_level = logging.WARN
+    if opts.verbose == 1:
+        log_level = logging.INFO
+    elif opts.verbose > 1:
+        log_level = log_level.DEBUG
+
+    if opts.quiet:
+        log_level = log_level.ERROR
+
+    logging.basicConfig(level=log_level)
 
     hash_func = functools.partial(file_hash, opts.hash)
     for path in opts.paths:

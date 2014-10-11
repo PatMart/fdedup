561cbb29ddd188f934a412cb397420a0a977e3f6
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-11 02:14:54 +0200

    Generalize tests.

--- itests/test_filename_in_path.py
+++ itests/test_filename_in_path.py
@@ -10,20 +10,26 @@ def normalize(groups):
     return sorted(map(sorted, groups))
 
 
-class Test(unittest.TestCase):
+tests = [
+    {
+        'args': ['./fdedup/fdedup.py', '--json', './static/chaplain', './static/chaplain.copy'],
+        'returncode': 0,
+        'stdout': json.dumps([
+            ['./static/chaplain', './static/chaplain.copy']
+        ]),
+        'stderr': ''
+    }
+]
 
+
+class Test(unittest.TestCase):
     def assertEqualNormalized(self, expected, actual):
         self.assertEqual(normalize(expected), normalize(actual))
 
     def test_filenames_in_path(self):
-        expected = [
-            ['./static/chaplain', './static/chaplain.copy']
-        ]
-
-        pipe = subprocess.Popen(['./fdedup/fdedup.py', '--json', './static/chaplain', './static/chaplain.copy'],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        out, err = pipe.communicate()
-        self.assertEqual(0, pipe.returncode)
-        self.assertEqualNormalized(expected, json.loads(out))
-        self.assertEqual('', err)
+        for test in tests:
+            pipe = subprocess.Popen(test['args'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = pipe.communicate()
+            self.assertEqual(test['returncode'], pipe.returncode)
+            self.assertEqualNormalized(json.loads(test['stdout']), json.loads(out))
+            self.assertEqual(test['stderr'], err)

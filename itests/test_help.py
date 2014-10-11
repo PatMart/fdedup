187867b5b64814b5af7987578cfb9415727f7c3c
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-11 02:30:02 +0200

    Test help & usage in more generic way.

--- itests/test_help.py
+++ itests/test_help.py
@@ -1,18 +1,33 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 import unittest
 import subprocess
 
+tests = [
+    {
+        'args': ['./fdedup/fdedup.py', '-h'],
+        'returncode': 0,
+        'stderr': ''
+    },
+    {
+        'args': ['./fdedup/fdedup.py', '--help'],
+        'returncode': 0,
+        'stderr': ''
+    },
+    {
+        'args': ['./fdedup/fdedup.py'],
+        'returncode': 2
+    },
+]
+
 
 class Test(unittest.TestCase):
-    def test_help(self):
-        with open(os.devnull, 'w') as devnull:
-            code = subprocess.call(['./fdedup/fdedup.py', '-h'], stdout=devnull, stderr=devnull)
-            self.assertEqual(0, code)
-
-    def test_help_long(self):
-        with open(os.devnull, 'w') as devnull:
-            code = subprocess.call(['./fdedup/fdedup.py', '--help'], stdout=devnull, stderr=devnull)
-            self.assertEqual(0, code)
+    def test(self):
+        for test in tests:
+            pipe = subprocess.Popen(test['args'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = pipe.communicate()
+            self.assertEqual(test['returncode'], pipe.returncode)
+            if 'stdout' in test:
+                self.assertEqual(test['stdout'], out)
+            if 'stderr' in test:
+                self.assertEqual(test['stderr'], err)

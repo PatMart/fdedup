e54b8eeda2263ab82eccb4c91095a0a0052aca14
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-13 21:26:49 +0200

    Don't cleanup environment by default.
    Whenever something goes wrong you want to have the *exact* env state
    that python had upon execution. So lets keep venv until the next run.

--- run_tests.sh
+++ run_tests.sh
@@ -11,7 +11,4 @@ pip install -r test-requirements.txt
 nosetests -v --with-xunit --all-modules --traverse-namespace --cover-xml \
         --with-xcoverage  --cover-package=fdedup --cover-inclusive --cover-erase
 coverage html
-if [ -d "venv" ]; then
-        rm -rf venv
-fi
 

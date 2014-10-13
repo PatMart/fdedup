9b6ef6a4cc2230f37b2e71d6d2289538e4004581
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-13 21:34:39 +0200

    Add PACKAGE variable.

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+PACKAGE=fdedup
+
 virtualenv venv
 
 . ./venv/bin/activate
 
 pip install -r test-requirements.txt
 nosetests -v --with-xunit --all-modules --traverse-namespace --cover-xml \
-        --with-xcoverage  --cover-package=fdedup --cover-inclusive --cover-erase
+        --with-xcoverage  --cover-package=$PACKAGE --cover-inclusive --cover-erase
 coverage html
 

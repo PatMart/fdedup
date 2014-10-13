d7c8c4b943c3f3cef61372f84e693e03ec638f01
Alexey Ulyanov <sibuser.nsk@gmail.com>
2014-10-13 21:12:29 +0200

    Put test requirements into a separate file.
    Fix #29

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
-if [ ! -d "venv" ]; then
-        virtualenv venv
+if [ -d "venv" ]; then
+    rm -rf venv
 fi
+virtualenv venv
 . venv/bin/activate
 
-pip install nosexcover
-pip install nose
-pip install coverage
+pip install -r test-requirements.txt
 nosetests -v --with-xunit --all-modules --traverse-namespace --cover-xml \
         --with-xcoverage  --cover-package=fdedup --cover-inclusive --cover-erase
 coverage html

2f194f15f0eb6ec77e6f4b13b929d5eee4c948cf
Alexey Ulyanov <sibuser.nsk@gmail.com>
2014-10-14 22:48:02 +0200

    Hardcoded version in the setup.py file.
    Fix #35

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import re
-
-
-def get_version(filename):
-    content = open(filename).read()
-    metadata = dict(re.findall("__(version)__ = '([^']+)'", content))
-    return metadata['version']
-
-
 from distutils.core import setup
 from setuptools import find_packages
 
@@ -16,7 +7,7 @@ setup(
     name='fdedup',
     packages=find_packages('fdedup', exclude=['static', 'tests', 'run_tests*',
                                               'requirements*']),
-    version=get_version('fdedup/__init__.py'),
+    version='0.0.3',
     description='File Deduplicator',
     author='Alexander Krasnukhin, Alexey Ulyanov',
     author_email='the.malkom@gmail.com, sibuser.nsk@gmail.com',

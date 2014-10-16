c0608218596969210a3da879ba105e5d71975300
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-16 23:37:34 +0200

    Random code rearrange.

--- fdedup/fdedup.py
+++ fdedup/fdedup.py
@@ -17,6 +17,39 @@ from iterate_files import iterate_files
 logger = logging.getLogger(__package__)
 
 
+def chunk_reader(fileobject, chunk_size):
+    while True:
+        chunk = fileobject.read(chunk_size)
+        if not chunk:
+            break
+        yield chunk
+
+
+def file_size(path, empty_as_none=False):
+    size = os.path.getsize(path)
+    if empty_as_none:
+        return size if size else None
+    else:
+        return size
+
+
+def file_hash(path, algorithm='md5', size=-1, chunk_size=65536):
+    try:
+        hasher = hashlib.new(algorithm)
+        with open(path, 'rb') as f:
+            read = 0
+            for chunk in chunk_reader(f, chunk_size):
+                read += len(chunk)
+                hasher.update(chunk)
+                if size != -1 and read >= size:
+                    break
+        return hasher.hexdigest()
+
+    except IOError as e:
+        logger.error(e)
+        return None
+
+
 def find_candidates(groups, func):
     candidates = {}
     for group in groups:
@@ -31,12 +64,9 @@ def find_candidates(groups, func):
     return (set(v) for v in candidates.itervalues() if len(v) > 1)
 
 
-def file_size(path, empty_as_none=False):
-    size = os.path.getsize(path)
-    if empty_as_none:
-        return size if size else None
-    else:
-        return size
+def verify_paths(paths):
+    for path in paths:
+        os.stat(path)
 
 
 def find_duplicates(paths, algorithm='md5', verify=False, ignore_empty=False):
@@ -72,36 +102,6 @@ def find_duplicates(paths, algorithm='md5', verify=False, ignore_empty=False):
                 logger.error('Hash collision detected: %s', ', '.join(sorted(group)))
         groups = groups_verified
     return groups
-
-
-def chunk_reader(fileobject, chunk_size):
-    while True:
-        chunk = fileobject.read(chunk_size)
-        if not chunk:
-            break
-        yield chunk
-
-
-def file_hash(path, algorithm='md5', size=-1, chunk_size=65536):
-    try:
-        hasher = hashlib.new(algorithm)
-        with open(path, 'rb') as f:
-            read = 0
-            for chunk in chunk_reader(f, chunk_size):
-                read += len(chunk)
-                hasher.update(chunk)
-                if size != -1 and read >= size:
-                    break
-        return hasher.hexdigest()
-
-    except IOError as e:
-        logger.error(e)
-        return None
-
-
-def verify_paths(paths):
-    for path in paths:
-        os.stat(path)
 
 
 def read_paths():

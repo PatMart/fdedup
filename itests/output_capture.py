d57cfcebcb36538fb88d515203f910888468c795
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-16 23:17:11 +0200

    Extract OutputCapture.

---
+++ itests/output_capture.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from StringIO import StringIO
+import sys
+
+
+class OutputCapture:
+    def __init__(self, stdin=None):
+        self._new = StringIO(stdin), StringIO(), StringIO()
+        self._old = (None, None, None)
+
+    def __enter__(self):
+        self._old = sys.stdin, sys.stdout, sys.stderr
+        sys.stdin, sys.stdout, sys.stderr = self._new
+        return self
+
+    def __exit__(self, *args):
+        sys.stdin, sys.stdout, sys.stderr = self._old
+
+    @property
+    def out(self):
+        return self._new[1].getvalue()
+
+    @property
+    def err(self):
+        return self._new[2].getvalue()
--- itests/tests.py
+++ itests/tests.py
@@ -2,23 +2,12 @@
 
 from contextlib import contextmanager
 import json
-import sys
-from StringIO import StringIO
+
+import testfixtures
 
 import config
 from fdedup import fdedup
-import testfixtures
-
-
-@contextmanager
-def capture_output(stdin):
-    new_in, new_out, new_err = StringIO(stdin), StringIO(), StringIO()
-    old_in, old_out, old_err = sys.stdin, sys.stdout, sys.stderr
-    try:
-        sys.stdin, sys.stdout, sys.stderr = new_in, new_out, new_err
-        yield sys.stdin, sys.stdout, sys.stderr
-    finally:
-        sys.stdin, sys.stdout, sys.stderr = old_in, old_out, old_err
+from itests.output_capture import OutputCapture
 
 
 @contextmanager
@@ -38,7 +27,7 @@ def check(spec):
     assert 'returncode' in spec
     with fixture(spec):
         with testfixtures.LogCapture(names='fdedup') as log:
-            with capture_output(spec.get('stdin', '')) as (inn, out, err):
+            with OutputCapture(stdin=spec.get('stdin', '')) as captured:
                 try:
                     code = fdedup.main(spec['args'])
                     raise SystemExit(code if code else 0)
@@ -48,16 +37,16 @@ def check(spec):
                 if 'stdout' in spec:
                     stdout = spec['stdout']
                     if stdout:
-                        assert normalize(json.loads(stdout)) == normalize(json.loads(out.getvalue()))
+                        assert normalize(json.loads(stdout)) == normalize(json.loads(captured.out))
                     else:  # empty stdout
-                        assert 0 == len(out.getvalue())
+                        assert 0 == len(captured.out)
 
                 if 'stderr' in spec:
                     stderr = spec['stderr']
                     if stderr:
-                        assert stderr == err.getvalue()
+                        assert stderr == captured.err
                     else:  # empty stderr
-                        assert 0 == len(err.getvalue())
+                        assert 0 == len(captured.err)
 
                 if 'stdlog' in spec:
                     stdlog = spec['stdlog']

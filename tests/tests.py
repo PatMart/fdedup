da0483755cdc533aaa4319c319e2c4053371cb42
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-16 23:24:51 +0200

    Reduce indentation.

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@
 from contextlib import contextmanager
 import json
 
-import testfixtures
+from testfixtures import LogCapture
 
 import config
 from fdedup import fdedup
@@ -25,34 +25,34 @@ def normalize(groups):
 def check(spec):
     assert 'args' in spec
     assert 'returncode' in spec
-    with fixture(spec):
-        with testfixtures.LogCapture(names='fdedup') as log:
-            with OutputCapture(stdin=spec.get('stdin', '')) as captured:
-                try:
-                    code = fdedup.main(spec['args'])
-                    raise SystemExit(code if code else 0)
-                except SystemExit as e:
-                    assert spec['returncode'] == e.code
+    with fixture(spec), \
+            LogCapture(names='fdedup') as log, \
+            OutputCapture(stdin=spec.get('stdin', '')) as captured:
+        try:
+            code = fdedup.main(spec['args'])
+            raise SystemExit(code if code else 0)
+        except SystemExit as e:
+            assert spec['returncode'] == e.code
 
-                if 'stdout' in spec:
-                    stdout = spec['stdout']
-                    if stdout:
-                        assert normalize(json.loads(stdout)) == normalize(json.loads(captured.out))
-                    else:  # empty stdout
-                        assert 0 == len(captured.out)
+        if 'stdout' in spec:
+            stdout = spec['stdout']
+            if stdout:
+                assert normalize(json.loads(stdout)) == normalize(json.loads(captured.out))
+            else:  # empty stdout
+                assert 0 == len(captured.out)
 
-                if 'stderr' in spec:
-                    stderr = spec['stderr']
-                    if stderr:
-                        assert stderr == captured.err
-                    else:  # empty stderr
-                        assert 0 == len(captured.err)
+        if 'stderr' in spec:
+            stderr = spec['stderr']
+            if stderr:
+                assert stderr == captured.err
+            else:  # empty stderr
+                assert 0 == len(captured.err)
 
-                if 'stdlog' in spec:
-                    stdlog = spec['stdlog']
-                    if stdlog:
-                        for l in stdlog:
-                            log.check(l)
+        if 'stdlog' in spec:
+            stdlog = spec['stdlog']
+            if stdlog:
+                for l in stdlog:
+                    log.check(l)
 
 
 def test_specs():

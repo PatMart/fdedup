61bc01158682173237c17824e3d28280972ace01
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-18 13:41:26 +0200

    Create TestSpec class.
    This way we no longer play with typos in dict keys. Everything is fixed
    on the language level.
    
    Fix #56

--- tests/config.py
+++ tests/config.py
@@ -3,220 +3,242 @@
 import json
 import os
 
-# {
-#     'args':           required, list of strings,  args array
-#     'returncode':     required, int,              expected return code
-#     'setup':          optional, lambda,           callable for setup
-#     'teardown':       optional, lambda,           callable for teardown
-#     'description':    optional, string,           description sentence starting with 'should'
-#     'stdin':          optional, string,           required input
-#     'stdout':         optional, string,           expected stdout
-#     'stderr':         optional, string,           expected stderr
-#     'stdlog':         optional, list of 3-tuples, expected log statements
-# },
-tests = [
-    {
-        'description': 'should fail and print usage by default',
-        'args': [],
-        'returncode': 2
-    },
-    {
-        'description': 'should print help with -h flag',
-        'args': ['-h'],
-        'returncode': 0,
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'description': 'should print help with --help flag',
-        'args': ['--help'],
-        'returncode': 0,
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'args': ['./static'],
-        'returncode': 0,
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'description': 'should return 1 whenever a non-existing path is provided mixed with existing',
-        'args': ['./static', 'moogoescow'],
-        'returncode': 1,
-        'stdout': None,
-        'stdlog': [
-            ('fdedup', 'ERROR', '[Errno 2] No such file or directory: \'moogoescow\'')
-        ],
-    },
-    {
-        'description': 'should return 1 whenever a non-existing path is provided',
-        'args': ['moogoescow'],
-        'returncode': 1,
-        'stdout': None,
-        'stdlog': [('fdedup', 'ERROR', '[Errno 2] No such file or directory: \'moogoescow\'')]
-    },
-    {
-        'description': 'should return 1 whenever a non-existing path is provided and quiet is set',
-        'args': ['--quiet', 'moogoescow'],
-        'returncode': 1,
-        'stdout': '',
-        'stderr': '',
-        'stdlog': None
-    },
-    {
-        'args': ['--json', 'static/chaplain', 'static/chaplain.copy'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/chaplain', 'static/chaplain.copy']
-        ]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'args': ['--json', 'static/chaplain', 'static/chaplain.modified'],
-        'returncode': 0,
-        'stdout': json.dumps([]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'args': ['--json', 'static/chaplain', 'static/chaplain.copy', 'static/chaplain.modified'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/chaplain', 'static/chaplain.copy']
-        ]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'args': ['--json', 'static/issue_9/ydg2DF', 'static/issue_9/A2VcHL'],
-        'returncode': 0,
-        'stdout': json.dumps([]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'description': 'should not ignore empty files by default',
-        'args': ['--json', 'static/empty', 'static/empty.copy'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/empty', 'static/empty.copy']
-        ]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'description': 'should ignore empty files if --ignore-empty is set',
-        'args': ['--ignore-empty', '--json', 'static/empty', 'static/empty.copy'],
-        'returncode': 0,
-        'stdout': json.dumps([]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'setup': lambda: os.chmod('static/issue_37/kawabanga', 0000),
-        'teardown': lambda: os.chmod('static/issue_37/kawabanga', 0644),
-        'description': 'should complain if permission denied',
-        'args': ['--json', 'static/issue_37'],
-        'returncode': 1,
-        'stdout': json.dumps([
-            ['static/issue_37/kawabanga.copy', 'static/issue_37/kawabanga.copy2']
-        ]),
-        'stdlog': [('fdedup', 'ERROR', '[Errno 13] Permission denied: \'static/issue_37/kawabanga\'')],
-    },
-    {
-        'description': 'should not duplicate duplicates if path is listed several times',
-        'args': ['--json', 'static/empty', 'static/empty', 'static/empty', 'static/empty.copy'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/empty', 'static/empty.copy']
-        ]),
-        'stderr': '',
-        'stdlog': None
-    },
-    {
-        'description': 'should work on normalized paths and understand redundant separators',
-        'args': ['--json', 'static/empty', './static/empty', '././static/empty', './static/issue_37/../empty', './static/empty.copy'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/empty', 'static/empty.copy']
-        ]),
-        'stderr': '',
-        'stdlog': None
-    },
-    {
-        'setup': lambda: os.link('static/issue_26/quote', 'static/issue_26/quote.hardlink'),
-        'teardown': lambda: os.remove('static/issue_26/quote.hardlink'),
-        'description': 'should treat hardlinks as separate files',
-        'args': ['--json', 'static/issue_26'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/issue_26/quote', 'static/issue_26/quote.copy', 'static/issue_26/quote.hardlink']
-        ]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'description': 'should incorrectly report duplicates on md5 collision',
-        'args': ['--algorithm', 'md5', '--json', 'static/issue_16'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/issue_16/hello', 'static/issue_16/erase']
-        ]),
-        'stderr': '',
-        'stdlog': None,
-    },
-    {
-        'description': 'should binary differentiate files with hash collision',
-        'args': ['--verify', '--algorithm', 'md5', '--json', 'static/issue_16'],
-        'returncode': 1,
-        'stdout': json.dumps([]),
-        'stdlog': [
-            ('fdedup', 'ERROR', 'Hash collision detected: %s' % ', '.join(sorted(['static/issue_16/erase', 'static/issue_16/hello'])))
-        ],
-    },
-    {
-        'description': 'should not affect true duplicates by verification',
-        'args': ['--verify', '--json', 'static/chaplain', 'static/chaplain.copy'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/chaplain', 'static/chaplain.copy']
-        ]),
-        'stderr': '',
-        'stdlog': None
-    },
-    {
-        'description': 'should take paths from stdin if requested',
-        'args': ['--json', '-'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/chaplain', 'static/chaplain.copy']
-        ]),
-        'stdin': '\n'.join(['static/chaplain', 'static/chaplain.copy']),
-        'stderr': '',
-        'stdlog': None
-    },
-    {
-        'description': 'should take paths from stdin if requested even mixed with filenames',
-        'args': ['--json', '-', 'static/bar/chaplain.copy2'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/chaplain', 'static/chaplain.copy', 'static/bar/chaplain.copy2']
-        ]),
-        'stdin': '\n'.join(['static/chaplain', 'static/chaplain.copy']),
-        'stderr': '',
-        'stdlog': None
-    },
-    {
-        'description': 'should ignore empty lines from stdin',
-        'args': ['--json', '-'],
-        'returncode': 0,
-        'stdout': json.dumps([
-            ['static/chaplain', 'static/chaplain.copy']
-        ]),
-        'stdin': '\n'.join(['', 'static/chaplain', 'static/chaplain.copy']),
-        'stderr': '',
-        'stdlog': None
-    },
-]
+from fdedup import fdedup
+
+
+class TestSpec(object):
+    """
+         args=          required, list of strings,  args array
+         returncode=    required, int,              expected return code
+         setup=         optional, lambda,           callable for setup
+         teardown=      optional, lambda,           callable for teardown
+         description=   optional, string,           description sentence starting with 'should'
+         stdin=         optional, string,           required input
+         stdout=        optional, string,           expected stdout
+         stderr=        optional, string,           expected stderr
+         stdlog=        optional, list of 3-tuples, expected log statements
+    """
+
+    def __init__(self, main, args, returncode, setup=None, teardown=None, description=None, stdin=None, stdout=None,
+                 stderr=None, stdlog=None):
+        self.main = main
+        self.args = args
+        self.returncode = returncode
+        self.setup = setup
+        self.teardown = teardown
+        self.description = description
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.stdlog = stdlog
+
+
+def get_tests():
+    return [
+        TestSpec(main=fdedup.main,
+                 description='should fail and print usage by default',
+                 args=[],
+                 returncode=2
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should print help with -h flag',
+                 args=['-h'],
+                 returncode=0,
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should print help with --help flag',
+                 args=['--help'],
+                 returncode=0,
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 args=['./static'],
+                 returncode=0,
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should return 1 whenever a non-existing path is provided mixed with existing',
+                 args=['./static', 'moogoescow'],
+                 returncode=1,
+                 stdout=None,
+                 stdlog=[
+                     ('fdedup', 'ERROR', '[Errno 2] No such file or directory: \'moogoescow\'')
+                 ],
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should return 1 whenever a non-existing path is provided',
+                 args=['moogoescow'],
+                 returncode=1,
+                 stdout=None,
+                 stdlog=[('fdedup', 'ERROR', '[Errno 2] No such file or directory: \'moogoescow\'')]
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should return 1 whenever a non-existing path is provided and quiet is set',
+                 args=['--quiet', 'moogoescow'],
+                 returncode=1,
+                 stdout='',
+                 stderr='',
+                 stdlog=None
+        ),
+        TestSpec(main=fdedup.main,
+                 args=['--json', 'static/chaplain', 'static/chaplain.copy'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/chaplain', 'static/chaplain.copy']
+                 ]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 args=['--json', 'static/chaplain', 'static/chaplain.modified'],
+                 returncode=0,
+                 stdout=json.dumps([]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 args=['--json', 'static/chaplain', 'static/chaplain.copy', 'static/chaplain.modified'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/chaplain', 'static/chaplain.copy']
+                 ]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 args=['--json', 'static/issue_9/ydg2DF', 'static/issue_9/A2VcHL'],
+                 returncode=0,
+                 stdout=json.dumps([]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should not ignore empty files by default',
+                 args=['--json', 'static/empty', 'static/empty.copy'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/empty', 'static/empty.copy']
+                 ]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should ignore empty files if --ignore-empty is set',
+                 args=['--ignore-empty', '--json', 'static/empty', 'static/empty.copy'],
+                 returncode=0,
+                 stdout=json.dumps([]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 setup=lambda: os.chmod('static/issue_37/kawabanga', 0000),
+                 teardown=lambda: os.chmod('static/issue_37/kawabanga', 0644),
+                 description='should complain if permission denied',
+                 args=['--json', 'static/issue_37'],
+                 returncode=1,
+                 stdout=json.dumps([
+                     ['static/issue_37/kawabanga.copy', 'static/issue_37/kawabanga.copy2']
+                 ]),
+                 stdlog=[('fdedup', 'ERROR', '[Errno 13] Permission denied: \'static/issue_37/kawabanga\'')],
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should not duplicate duplicates if path is listed several times',
+                 args=['--json', 'static/empty', 'static/empty', 'static/empty', 'static/empty.copy'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/empty', 'static/empty.copy']
+                 ]),
+                 stderr='',
+                 stdlog=None
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should work on normalized paths and understand redundant separators',
+                 args=['--json', 'static/empty', './static/empty', '././static/empty', './static/issue_37/../empty',
+                       './static/empty.copy'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/empty', 'static/empty.copy']
+                 ]),
+                 stderr='',
+                 stdlog=None
+        ),
+        TestSpec(main=fdedup.main,
+                 setup=lambda: os.link('static/issue_26/quote', 'static/issue_26/quote.hardlink'),
+                 teardown=lambda: os.remove('static/issue_26/quote.hardlink'),
+                 description='should treat hardlinks as separate files',
+                 args=['--json', 'static/issue_26'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/issue_26/quote', 'static/issue_26/quote.copy', 'static/issue_26/quote.hardlink']
+                 ]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should incorrectly report duplicates on md5 collision',
+                 args=['--algorithm', 'md5', '--json', 'static/issue_16'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/issue_16/hello', 'static/issue_16/erase']
+                 ]),
+                 stderr='',
+                 stdlog=None,
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should binary differentiate files with hash collision',
+                 args=['--verify', '--algorithm', 'md5', '--json', 'static/issue_16'],
+                 returncode=1,
+                 stdout=json.dumps([]),
+                 stdlog=[
+                     ('fdedup', 'ERROR',
+                      'Hash collision detected: %s' % ', '.join(sorted(['static/issue_16/erase', 'static/issue_16/hello'])))
+                 ],
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should not affect true duplicates by verification',
+                 args=['--verify', '--json', 'static/chaplain', 'static/chaplain.copy'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/chaplain', 'static/chaplain.copy']
+                 ]),
+                 stderr='',
+                 stdlog=None
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should take paths from stdin if requested',
+                 args=['--json', '-'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/chaplain', 'static/chaplain.copy']
+                 ]),
+                 stdin='\n'.join(['static/chaplain', 'static/chaplain.copy']),
+                 stderr='',
+                 stdlog=None
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should take paths from stdin if requested even mixed with filenames',
+                 args=['--json', '-', 'static/bar/chaplain.copy2'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/chaplain', 'static/chaplain.copy', 'static/bar/chaplain.copy2']
+                 ]),
+                 stdin='\n'.join(['static/chaplain', 'static/chaplain.copy']),
+                 stderr='',
+                 stdlog=None
+        ),
+        TestSpec(main=fdedup.main,
+                 description='should ignore empty lines from stdin',
+                 args=['--json', '-'],
+                 returncode=0,
+                 stdout=json.dumps([
+                     ['static/chaplain', 'static/chaplain.copy']
+                 ]),
+                 stdin='\n'.join(['', 'static/chaplain', 'static/chaplain.copy']),
+                 stderr='',
+                 stdlog=None
+        ),
+    ]
--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -6,16 +6,15 @@ import json
 from testfixtures import LogCapture
 
 import config
-from fdedup import fdedup
 from output_capture import OutputCapture
 
 
 @contextmanager
 def fixture(spec):
     try:
-        yield spec['setup']() if 'setup' in spec else None
+        yield spec.setup() if spec.setup else None
     finally:
-        spec['teardown']() if 'teardown' in spec else None
+        spec.teardown() if spec.teardown else None
 
 
 def normalize(groups):
@@ -23,43 +22,42 @@ def normalize(groups):
 
 
 def check(spec):
-    assert 'args' in spec
-    assert 'returncode' in spec
+    lognames = []
+    if spec.stdlog is not None:
+        for l in spec.stdlog:
+            lognames.append(l[0])
+
     with fixture(spec), \
-            LogCapture(names='fdedup') as log, \
-            OutputCapture(stdin=spec.get('stdin', '')) as captured:
+            LogCapture(names=','.join(lognames)) as log, \
+            OutputCapture(stdin=spec.stdin) as captured:
         try:
-            code = fdedup.main(spec['args'])
+            code = spec.main(spec.args)
             raise SystemExit(code if code else 0)
         except SystemExit as e:
-            assert spec['returncode'] == e.code
+            assert spec.returncode == e.code
 
-        if 'stdout' in spec:
-            stdout = spec['stdout']
-            if stdout:
-                assert normalize(json.loads(stdout)) == normalize(json.loads(captured.out))
+        if spec.stdout is not None:
+            if spec.stdout:
+                assert normalize(json.loads(spec.stdout)) == normalize(json.loads(captured.out))
             else:  # empty stdout
                 assert 0 == len(captured.out)
 
-        if 'stderr' in spec:
-            stderr = spec['stderr']
-            if stderr:
-                assert stderr == captured.err
+        if spec.stderr is not None:
+            if spec.stderr:
+                assert spec.stderr == captured.err
             else:  # empty stderr
                 assert 0 == len(captured.err)
 
-        if 'stdlog' in spec:
-            stdlog = spec['stdlog']
-            if stdlog:
-                for l in stdlog:
-                    log.check(l)
+        if spec.stdlog is not None:
+            for l in spec.stdlog:
+                log.check(l)
 
 
 def test_specs():
-    for spec in config.tests:
+    for spec in config.get_tests():
         checker = lambda x: check(x)
-        if 'description' in spec:
-            checker.description = spec['description']
+        if spec.description is not None:
+            checker.description = spec.description
         else:
-            checker.description = ' '.join(['fdedup'] + spec['args'])
+            checker.description = ' '.join(str(spec.main) + str(spec.args))
         yield checker, spec

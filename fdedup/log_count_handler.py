7ff9992ddda621e68a7fdb23ecbe758cbd8a1391
Alexander Krasnukhin <the.malkolm@gmail.com>
2014-10-20 23:04:49 +0200

    Use defaultdict instead of extra key logic.

--- fdedup/log_count_handler.py
+++ fdedup/log_count_handler.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+
 import logging
 
 
 class LogCountHandler(logging.Handler):
     def __init__(self):
         super(LogCountHandler, self).__init__()
-        self._counter = {}
+        self._counter = defaultdict(int)
 
     def emit(self, record):
         for lvl in record.levelno, record.levelname:
-            if lvl not in self._counter:
-                self._counter[lvl] = 1
-            else:
-                self._counter[lvl] += 1
+            self._counter[lvl] += 1
 
     def count(self, lvl):
         return self._counter.get(lvl, 0)
